af6e39544f7bde3619412810b2906bf511bf7a30
GabriellMonteiro <gabrielbrava@outlook.com.br>
2023-05-14 20:52:05 -0300

    adicionado metodo tempoDeEspera e organização das strings

--- ElementOOPs-master/application/Batalha.java
+++ ElementOOPs-master/application/Batalha.java
@@ -5,11 +5,11 @@
 
 package application;
 
-import entites.BreezeHacker;
-import entites.BurnCoder;
-import entites.Criatura;
-import entites.StoneDev;
-import entites.WaveNerd;
+import criatura.BreezeHacker;
+import criatura.BurnCoder;
+import criatura.Criatura;
+import criatura.StoneDev;
+import criatura.WaveNerd;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -36,28 +36,24 @@ public class Batalha {
         if (escolha >= 1 && escolha <= 5) {
             switch (escolha) {
                 case 1:
-                    System.out.println("Você escolheu o StoneDev");
                     criaturas[0] = new StoneDev();
                     criaturas[1] = new WaveNerd();
                     criaturas[2] = new BurnCoder();
                     criaturas[3] = new BreezeHacker();
                     break;
                 case 2:
-                    System.out.println("Você escolheu o WaveNerd");
                     criaturas[0] = new WaveNerd();
                     criaturas[1] = new StoneDev();
                     criaturas[2] = new BurnCoder();
                     criaturas[3] = new BreezeHacker();
                     break;
                 case 3:
-                    System.out.println("Você escolheu o BurnCoder");
                     criaturas[0] = new BurnCoder();
                     criaturas[1] = new WaveNerd();
                     criaturas[2] = new StoneDev();
                     criaturas[3] = new BreezeHacker();
                     break;
                 case 4:
-                    System.out.println("Você escolheu o BreezeHacker");
                     criaturas[0] = new BreezeHacker();
                     criaturas[1] = new WaveNerd();
                     criaturas[2] = new StoneDev();
@@ -76,7 +72,14 @@ public class Batalha {
     }
 
     public void iniciaABatalha() {
+        System.out.println("CRIAUTURA ESCOLHIDA:");
+        System.out.println(criaturas[0].fraseDeEfeito(criaturas[0]));
+        tempoDeEspera();
         System.out.println(criaturas[0].caracteristicas(criaturas[0]));
+        tempoDeEspera();
+        System.out.println( criaturaInimiga + "º ADVERSÁRIO: ");
+        System.out.println(criaturas[criaturaInimiga].fraseDeEfeito(criaturas[criaturaInimiga]));
+        tempoDeEspera();
         System.out.println(criaturas[criaturaInimiga].caracteristicas(criaturas[criaturaInimiga]));
         System.out.println("1. iniciar o torneio \n" +
                 "2. Sair do programa");
@@ -99,10 +102,6 @@ public class Batalha {
     }
 
     public void batalha() {
-        //if pra verificar a vida dos oponentes e retornar a vida
-
-
-        // verifica se a criatura escolhida é mais veloz que o adversario
         if (criaturas[0].getVELOCIDADE() >= criaturas[criaturaInimiga].getVELOCIDADE()) {
             escolhido(criaturas[0], criaturas[criaturaInimiga] );
             if(this.statusDaBatalha() == false){
@@ -126,8 +125,9 @@ public class Batalha {
                 "3. Sair do programa");
         int escolha = this.sc.nextInt();
         sc.nextLine();
-        //ADICIONEI UMA LINHA PRA MOSTRAR O ATAQUE
-        System.out.println("Seu ataque");
+
+        System.out.println();
+        System.out.println("SEU ATAQUE: ");
         switch (escolha) {
             case 1:
                 atacante.ataqueFisico(atacante, defensor);
@@ -138,14 +138,14 @@ public class Batalha {
             case 3:
                 System.out.println("Processando..");
                 System.out.println("Jogo finalizado!");
-                return;
+                break;
         }
 
     }
 
     public void inimigo(Criatura atacante, Criatura defensor){
         //ADICIONEI UMA LINHA PRA MOSTRAR O ATAQUE
-        System.out.println("Ataque inimigo");
+        System.out.println("ATAQUE INIMIGO: ");
         Random rand = new Random();
         int escolha = rand.nextInt(2) + 1;
         switch (escolha) {
@@ -162,31 +162,40 @@ public class Batalha {
         return criaturas[0].getPONTOS_DE_VIDA() <= 0 || criaturas[criaturaInimiga].getPONTOS_DE_VIDA() <= 0;
     }
     public static void resetarPontoDeVida(){
-
         criaturas[0].setPONTOS_DE_VIDA(criaturas[0].getPONTOS_DE_VIDA());
     }
     public void continuarTorneio(){
         if (criaturas[0].getPONTOS_DE_VIDA() <= 0) {
-            System.out.println("Você perdeu o torneio");
-            criaturaInimiga = 1;
-            this.escolhaCriatura();
+            System.out.println("VOCÊ FOI ELIMINADO! ");
 
         } else if (criaturas[criaturaInimiga].getPONTOS_DE_VIDA() <= 0){
-            System.out.println("Você ganhou");
-            resetarPontoDeVida();
-            System.out.println("SUA VIDA FOI REGENERADA " + criaturas[0].getPONTOS_DE_VIDA());
             System.out.println("LUTA ENCERRADA");
+            System.out.println(criaturas[0].getNOME().toUpperCase() + " VENCEU A " + criaturaInimiga + "ª RODADA");
+            resetarPontoDeVida();
+            System.out.println();
+            System.out.println("SUA VIDA FOI REGENERADA " + criaturas[0].getPONTOS_DE_VIDA());
+
+            System.out.println();
 
             ++criaturaInimiga;
             if (criaturaInimiga >= criaturas.length) {
-                System.out.println("VOCÊ GANHOU TORNEIO");
+                System.out.println( criaturas[0].getNOME().toUpperCase() + " GANHOU  O TORNEIO");
+                System.out.println(criaturas[0].fraseDeEfeito(criaturas[0]));
                 criaturaInimiga = 1;
-                this.escolhaCriatura();
+                System.out.println("\n" +
+                        "1. INICIAR UM NOVO TORNEIO" +
+                        "2. ENCERRAR APLICAÇÃO");
+                int escolha = sc.nextInt();
+                switch (escolha) {
+                    case 1:
+                        this.escolhaCriatura();
+                        break;
+                    case 2:
+                        break;
+                }
             } else {
-                // Chamar a proxima batalha
                 this.iniciaABatalha();
             }
-
         }
     }
     public void continuarBatalha(){
@@ -196,5 +205,16 @@ public class Batalha {
         }else{
             this.continuarTorneio();
         }
+    }
+    public void tempoDeEspera(){
+     try {
+        Thread.sleep(2000); // aguarda 1 segundo
+    } catch (InterruptedException e) {
+         System.out.println("Ocorreu um erro inesperado." +
+                 "Reinicializando o programa...");
+         tempoDeEspera();
+         Menu menu = new Menu();
+         menu.iniciarJogo();
+     }
     }
 }
